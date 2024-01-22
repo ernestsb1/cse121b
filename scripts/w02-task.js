68831569bd1b90de51337763d85cc417b72283ba
ernestsb1 <ernestnyore2020@gmail.com>
2024-01-23 00:21:20 +0100

    Added

---
+++ scripts/w02-task.js
@@ -0,0 +1,43 @@
+/* W02-Task - Profile Home Page */
+let fullName = "Ernest Ohwojeheri";
+/* Step 1 - Setup type tasks - no code required */
+
+const currentYear = 2024;
+
+/* Step 2 - Variables */
+
+const profilePicture = "images/my image .jpg";
+
+
+
+/* Step 3 - Element Variables */
+
+const foodElement = document.getElementById("food")
+
+const yearElement = document.querySelector("#year");
+
+const imageElement = document.getElementById("profile-image");
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = "<strong> ${fullName}</strong>";
+
+yearElement.textContent = currentYear;
+
+imageElement.setAttribute("src", profilePicture);
+
+
+
+
+
+
+/* Step 5 - Array */
+
+const favoriteFoods = ["Pizza", "Pasta",  "Lasagne", "Gelato (Ice cream)",];
+
+const favoriteFood = "Ice cream";
+
+
+
+
